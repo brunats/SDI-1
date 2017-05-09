96453f8828949ea5a57aad626c63166323fabd28
Forvisk <adrianojr11@gmail.com>
2017-05-09 09:41:24 -0300

    File, Quota e inicio de Download

--- T_09_Dropbox/SDI_Dropbox_T08/src/main/java/com/mycompany/sdi_dropbox_t08/SDIFirstTest.java
+++ T_09_Dropbox/SDI_Dropbox_T08/src/main/java/com/mycompany/sdi_dropbox_t08/SDIFirstTest.java
@@ -5,9 +5,11 @@
  */
 package com.mycompany.sdi_dropbox_t08;
 
+import com.dropbox.core.DbxDownloader;
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.*;
 import com.dropbox.core.v2.files.FileMetadata;
 import com.dropbox.core.v2.files.FolderMetadata;
 import com.dropbox.core.v2.files.ListFolderResult;
@@ -15,14 +17,16 @@ import com.dropbox.core.v2.files.Metadata;
 import com.dropbox.core.v2.users.FullAccount;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class SDIFirstTest {    
     
     private static final String ACCESS_TOKEN = "cAOcO6AuOGAAAAAAAAABiHFP5z4mOpd3pKHfuJ8q8xL2kep6FP50ATaILd70_zvb";
+    private static final String NOVAPASTA = "/Pasta04";
+    private static final String NOVOARQUIVO = "/NovoArquivo.txt";
+    private static final String ARQUIVODOWLOAD = "/NovoArquivo.txt";
     
     
     public static void main(String args[]) throws DbxException, IOException {
@@ -48,21 +52,65 @@ public class SDIFirstTest {
 
             result = client.files().listFolderContinue(result.getCursor());
         }
-
+        /*
         // Upload "test.txt" to Dropbox
         try (InputStream in = new FileInputStream("/tmp/test.txt")) {
             FileMetadata metadata = client.files().uploadBuilder("/test.txt")
                 .uploadAndFinish(in);
         }
-        
-        criarPasta("Teste", client);
+        */
+        criarArquivo(NOVOARQUIVO, client);
+        //criarPasta(NOVAPASTA, client);
+        quotaUsuario(client);
     } 
     
-    private static boolean criarPasta(String nomePasta, DbxClientV2 client) throws FileNotFoundException, IOException{
+    private static boolean criarPasta(String nomePasta, DbxClientV2 cliente) throws CreateFolderErrorException, DbxException, IOException{
         
-        FolderMetadata metadata = new client.files().
+       /* FolderMetadata metadata;
+        metadata = */cliente.files().createFolder(nomePasta);
         
         
         return true;
     }
+    
+    private static boolean criarArquivo( String nomeArquivo, DbxClientV2 cliente) throws FileNotFoundException, IOException, DbxException{
+        // Upload "test.txt" to Dropbox
+        try (InputStream in = new FileInputStream("/tmp/test.txt")) {
+            /*FileMetadata metadata;
+            metadata = */
+            FileMetadata uploadAndFinish = cliente.files().uploadBuilder(nomeArquivo)
+                    .uploadAndFinish(in);
+        }
+        return true;
+    }
+    
+    
+    private static boolean downloadArquivo( String nomeArquivo, DbxClientV2 cliente) throws FileNotFoundException, DbxException, IOException{
+        DbxDownloader<FileMetadata> downloader;
+        downloader = cliente.files().downloadBuilder(nomeArquivo).start();
+        
+        /*
+        try (FileOutputStream out = new FileOutputStream("/tmp/download.txt")) {
+            FileMetadata response = downloader.getResult();
+            InputStream in = downloader.getInputStream();
+            // read from in, write to out
+        } finally {
+            downloader.close();
+        }
+        */
+        try (FileOutputStream out = new FileOutputStream("/tmp/download.txt")) {
+            FileMetadata response;
+            response = downloader.download(out);
+        }
+        return true;
+    }
+    
+    
+    private static boolean quotaUsuario( DbxClientV2 cliente) throws DbxException{
+        String quota = cliente.users().getSpaceUsage().toStringMultiline();
+        System.out.print(quota);
+        return true;
+    }
+    
+    
 }
