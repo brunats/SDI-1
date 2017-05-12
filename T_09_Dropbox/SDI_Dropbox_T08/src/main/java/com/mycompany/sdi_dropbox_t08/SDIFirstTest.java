809346073f5260722b6c5d1efc4bff89b29e9f61
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2017-05-12 09:57:50 -0300

    Add files via upload
    Incluso download e funcionando :D, necessário alterar local das variaveis e token :D

--- T_09_Dropbox/SDI_Dropbox_T08/src/main/java/com/mycompany/sdi_dropbox_t08/SDIFirstTest.java
+++ T_09_Dropbox/SDI_Dropbox_T08/src/main/java/com/mycompany/sdi_dropbox_t08/SDIFirstTest.java
@@ -3,42 +3,54 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.sdi_dropbox_t08;
+package com.mycompanyodl3.sdibeansdb;
 
-import com.dropbox.core.DbxDownloader;
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
 import com.dropbox.core.v2.files.*;
-import com.dropbox.core.v2.files.FileMetadata;
-import com.dropbox.core.v2.files.FolderMetadata;
-import com.dropbox.core.v2.files.ListFolderResult;
-import com.dropbox.core.v2.files.Metadata;
 import com.dropbox.core.v2.users.FullAccount;
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class SDIFirstTest {    
+    private static final String ACCESS_TOKEN = "S5qD3HLfptkAAAAAAAAFAs5onbHxjeWsTpIqQRV8CMq7b8HI-GfiXncSFhp_Y3Qn";
     
-    private static final String ACCESS_TOKEN = "cAOcO6AuOGAAAAAAAAABiHFP5z4mOpd3pKHfuJ8q8xL2kep6FP50ATaILd70_zvb";
-    private static final String NOVAPASTA = "/Pasta04";
-    private static final String NOVOARQUIVO = "/NovoArquivo.txt";
-    private static final String ARQUIVODOWLOAD = "/NovoArquivo.txt";
+    private static final String name_Arq_Up = "/teste.txt";
+    private static final String adress_Arq_Up = "/home/bruna/NetBeansProjects/SDIBeansDB/teste.txt";
+    
+    private static final String name_Dir = "/pastaTeste";
+    private static String quota = "";
+    
+    private static final String adress_Arq_Down = "/home/bruna/NetBeansProjects/SDIBeansDB/baixadooooo.txt";
     
     
     public static void main(String args[]) throws DbxException, IOException {
         // Create Dropbox client
-        DbxRequestConfig config;
-        config = new DbxRequestConfig("dropbox/java-tutorial", "en_US");
+        DbxRequestConfig config = new DbxRequestConfig("dropbox/java-tutorial", "en_US");
         DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
 
         // Get current account info
         FullAccount account = client.users().getCurrentAccount();
         System.out.println(account.getName().getDisplayName());
 
+        System.out.println("testando criar arquivo");
+            enviarArquivo(name_Arq_Up, adress_Arq_Up, client);
+        System.out.println("testando criar diretorio");
+            criaDiretorio(name_Dir, client);
+        System.out.println("testando quota");
+            quota = quotaClient(client);
+            System.out.println(quota);
+        System.out.println("testando download arquivo");
+            receberArquivo("/testeD.txt",client);
+        
+    }
+    
+    private static void enviarArquivo(String arq, String end, DbxClientV2 client) throws DbxException, IOException{
         // Get files and folder metadata from Dropbox root directory
         ListFolderResult result = client.files().listFolder("");
         while (true) {
@@ -52,65 +64,43 @@ public class SDIFirstTest {
 
             result = client.files().listFolderContinue(result.getCursor());
         }
-        /*
+
         // Upload "test.txt" to Dropbox
-        try (InputStream in = new FileInputStream("/tmp/test.txt")) {
-            FileMetadata metadata = client.files().uploadBuilder("/test.txt")
+        try (InputStream in = new FileInputStream(end)) {
+            FileMetadata metadata = client.files().uploadBuilder(arq)
                 .uploadAndFinish(in);
         }
-        */
-        criarArquivo(NOVOARQUIVO, client);
-        //criarPasta(NOVAPASTA, client);
-        quotaUsuario(client);
-    } 
-    
-    private static boolean criarPasta(String nomePasta, DbxClientV2 cliente) throws CreateFolderErrorException, DbxException, IOException{
-        
-       /* FolderMetadata metadata;
-        metadata = */cliente.files().createFolder(nomePasta);
-        
-        
-        return true;
     }
     
-    private static boolean criarArquivo( String nomeArquivo, DbxClientV2 cliente) throws FileNotFoundException, IOException, DbxException{
-        // Upload "test.txt" to Dropbox
-        try (InputStream in = new FileInputStream("/tmp/test.txt")) {
-            /*FileMetadata metadata;
-            metadata = */
-            FileMetadata uploadAndFinish = cliente.files().uploadBuilder(nomeArquivo)
-                    .uploadAndFinish(in);
-        }
-        return true;
+    private static void criaDiretorio(String n_Dir, DbxClientV2 client) throws DbxException, IOException{
+        FolderMetadata createFolder;
+        createFolder = client.files().createFolder(n_Dir);
     }
     
+    private static String quotaClient(DbxClientV2 client) throws DbxException{
+        quota = client.users().getSpaceUsage().toStringMultiline();
+        return quota;
+    }
     
-    private static boolean downloadArquivo( String nomeArquivo, DbxClientV2 cliente) throws FileNotFoundException, DbxException, IOException{
-        DbxDownloader<FileMetadata> downloader;
-        downloader = cliente.files().downloadBuilder(nomeArquivo).start();
+    private static void receberArquivo(String path, DbxClientV2 client) throws DbxException, IllegalArgumentException{
+        try{//output file for download --> storage location on local system to download file
+                OutputStream downloadFile = new FileOutputStream(adress_Arq_Down);
+                try{
+                    FileMetadata metadata = client.files().downloadBuilder(path)
+                        .download(downloadFile);
+                }
+                finally{
+                    downloadFile.close();
+                }
+            }//exception handled
         
-        /*
-        try (FileOutputStream out = new FileOutputStream("/tmp/download.txt")) {
-            FileMetadata response = downloader.getResult();
-            InputStream in = downloader.getInputStream();
-            // read from in, write to out
-        } finally {
-            downloader.close();
-        }
-        */
-        try (FileOutputStream out = new FileOutputStream("/tmp/download.txt")) {
-            FileMetadata response;
-            response = downloader.download(out);
-        }
-        return true;
-    }
-    
-    
-    private static boolean quotaUsuario( DbxClientV2 cliente) throws DbxException{
-        String quota = cliente.users().getSpaceUsage().toStringMultiline();
-        System.out.print(quota);
-        return true;
+            catch (DbxException | IOException e){//error downloading file
+               System.out.println("Unable to download file to local system\n Error: " + e);
+            }
+        //error downloading file
+
     }
     
     
 }
+
